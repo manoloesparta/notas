e5bcf0d657cbd811b46d5dea441958bb9af4fe9f
Manolo <manolototopoyo@hotmail.com>
2017-08-16 13:23:37 -0700

    bash

---
+++ linux/bash/op.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -r NUM1=5
+
+declare -r num2=4
+
+num3=$((NUM1+num2))
+num4=$((NUM1-num2))
+num5=$((NUM1*num2))
+num6=$((NUM1/num2))
+
+echo "5 + 4 = $num3"
+echo "5 - 4 = $num4"
+echo "5 * 4 = $num5"
+echo "5 / 4 = $num6"
